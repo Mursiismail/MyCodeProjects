375931cd36bc460606a4a7024a7ff4905905b990
mursi.ismail <morsi.iceat@gmail.com>
2025-06-18 15:41:09 +0100

    end of the day 3 Whedensday

---
+++ 09stract/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+	type Student struct {
+		Name  string
+		school string
+		year	int 
+	}
+
+	func main() {
+		student1 := Student{
+			Name:  "Mursi",
+			school: "University of Science and Technology",
+			year:  2024,
+		}
+
+		fmt.Println("tudent informaition")
+		fmt.Println("Name:", student1.Name)
+		fmt.Println("School:", student1.school)
+		fmt.Println("Year:", student1.year)
+	}
+	
+
+		// Accessing the fields directly
