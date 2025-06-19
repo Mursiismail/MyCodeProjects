4a17176a8161c1ccf1b2dc64aae317145a7ed466
mursiismai <morsi.iceat@gmail.com>
2025-06-19 15:29:06 +0100

    thharsday 3 pm

---
+++ 10exr/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main () {
+
+	var number int
+
+ fmt.Println("Enter your favorite Number :")	
+ reader := bufio.NewReader(os.Stdin)
+ input, _ := reader.ReadString('\n')
+ input = strings.TrimSpace(input)
+ 
+ 
+input = number 
+
+
+
+
+
+
+
+}
---
+++ 13myPkg/go.mod
@@ -0,0 +1,5 @@
+module myPkg
+
+go 1.24.0
+
+require github.com/01-edu/z01 v0.1.0 // indirect
